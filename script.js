7c1fed8b1b7d26935885a422e200a41e3dbebf60
RenVoO <ahmadfahrifauzan@gmail.com>
2025-02-19 21:40:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah terkirim!');
+    // Anda bisa menambahkan kode untuk mengirim data formulir ke server di sini
+});
